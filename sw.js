b4f680e35906560eeb03648511f9e10a5440514b
ItsMeEndemanset <bruhmoments133417@gmail.com>
2026-04-05 21:27:39 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,15 +6,15 @@ self.addEventListener("install", (event) => {
       return cache.addAll([
         "/",
         "/index.html",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files-/examdee.ai-vietnam.vn/assets/index-BUoCWu2I.js",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files-/examdee.ai-vietnam.vn/assets/index-Bcm3ID3h.css",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files-/examdee.ai-vietnam.vn/offline-api/user.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files-/examdee.ai-vietnam.vn/offline-api/lessons.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files-/examdee.ai-vietnam.vn/offline-api/pronunciation.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files-/examdee.ai-vietnam.vn/offline-api/progress.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files-/examdee.ai-vietnam.vn/offline-api/live.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files-/examdee.ai-vietnam.vn/offline-api/exam.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files-/examdee.ai-vietnam.vn/offline-api/common.json"
+        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/assets/index-BUoCWu2I.js",
+        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/assets/index-Bcm3ID3h.css",
+        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/user.json",
+        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/lessons.json",
+        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/pronunciation.json",
+        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/progress.json",
+        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/live.json",
+        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/exam.json",
+        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/common.json"
       ]);
     })
   );

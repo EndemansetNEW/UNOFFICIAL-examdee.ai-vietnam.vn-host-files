8e8bb6a84d0964182e1862b4a98dad6186e9ed3f
ItsMeEndemanset <bruhmoments133417@gmail.com>
2026-04-05 21:54:14 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,17 +4,17 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE).then((cache) => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/assets/index-BUoCWu2I.js",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/assets/index-Bcm3ID3h.css",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/user.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/lessons.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/pronunciation.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/progress.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/live.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/exam.json",
-        "/UNOFFICIAL-examdee.ai-vietnam.vn-host-files/examdee.ai-vietnam.vn/offline-api/common.json"
+        "./",
+        "./index.html",
+        "./examdee.ai-vietnam.vn/assets/index-BUoCWu2I.js",
+        "./examdee.ai-vietnam.vn/assets/index-Bcm3ID3h.css",
+        "./examdee.ai-vietnam.vn/offline-api/user.json",
+        "./examdee.ai-vietnam.vn/offline-api/lessons.json",
+        "./examdee.ai-vietnam.vn/offline-api/pronunciation.json",
+        "./examdee.ai-vietnam.vn/offline-api/progress.json",
+        "./examdee.ai-vietnam.vn/offline-api/live.json",
+        "./examdee.ai-vietnam.vn/offline-api/exam.json",
+        "./examdee.ai-vietnam.vn/offline-api/common.json"
       ]);
     })
   );
